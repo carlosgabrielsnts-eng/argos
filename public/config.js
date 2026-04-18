448a827a900f1ceff696b7c86e294ee287d12d8d
carlosgabrielsnts-eng <carlosgabrielsnts@gmail.com>
2026-04-18 17:21:54 -0300

    debug: improve env logging

--- public/config.js
+++ public/config.js
@@ -5,7 +5,7 @@ window.CONFIG = {
   },
   discord: {
     clientId: "1489509789305405600",
-    redirectUri: `${window.location.origin}/login.html`,
+    redirectUri: `${window.location.origin}/auth/discord/callback`,
     scope: "identify email"
   },
   mercadopago: {
